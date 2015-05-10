02199adab6e06c29a9b2bac1f100608333284dfc
Artur Orzeł <artekorzel@gmail.com>
2015-05-10 16:15:19 +0200

    scripts

--- dispatch-rider/run_slave.sh
+++ dispatch-rider/run_slave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host localhost -port 1099 -agents InfoAgent:dtp.jade.info.InfoAgent
+R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host arto.no-ip.info -port 1099 -agents InfoAgent:dtp.jade.vm.VMAgent
