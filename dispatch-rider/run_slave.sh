1e51cb3e28bd7d130409db8abf6183faca3786c5
Artur Orzeł <artekorzel@gmail.com>
2015-05-11 21:50:11 +0200

    scripts vm names

--- dispatch-rider/run_slave.sh
+++ dispatch-rider/run_slave.sh
@@ -2,4 +2,4 @@
 
 LOCAL_HOST=`ifconfig | grep inet | awk '{print $2}' | cut -d ':' -f 2 | grep ^10`
 
-R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host arto.no-ip.info -port 1099 -local-host $LOCAL_HOST -agents VMAgent:dtp.jade.vm.VMAgent
+R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host arto.no-ip.info -port 1099 -agents VMAgent_${LOCAL_HOST}:dtp.jade.vm.VMAgent
