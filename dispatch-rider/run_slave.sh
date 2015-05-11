2924e39bbaf6bd3153778128e735fcc09510a39e
Artur Orzeł <artekorzel@gmail.com>
2015-05-11 21:33:37 +0200

    scripts

--- dispatch-rider/run_slave.sh
+++ dispatch-rider/run_slave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host arto.no-ip.info -port 1099 -agents InfoAgent:dtp.jade.vm.VMAgent
+R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host arto.no-ip.info -port 1099 -agents VMAgent:dtp.jade.vm.VMAgent
---
+++ dispatch-rider/run_submain.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host arto.no-ip.info -port 1099 -agents "DistributorAgent:dtp.jade.distributor.DistributorAgent;CrisisManagerAgent:dtp.jade.crisismanager.CrisisManagerAgent;InfoAgent:dtp.jade.info.InfoAgent"
