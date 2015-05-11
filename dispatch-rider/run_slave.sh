607ce7e337e106517dfff4eb1e08b151e114fc98
Artur Orzeł <artekorzel@gmail.com>
2015-05-11 21:46:48 +0200

    scripts ips

--- dispatch-rider/run_slave.sh
+++ dispatch-rider/run_slave.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host arto.no-ip.info -port 1099 -agents VMAgent:dtp.jade.vm.VMAgent
+LOCAL_HOST=`ifconfig | grep inet | awk '{print $2}' | cut -d ':' -f 2 | grep ^10`
+
+R_HOME=/usr/lib/R java -jar ./build/libs/dispatch-rider-1.0.0-standalone.jar -Xmx2048m -Djava.library.path=./lib/ -container -host arto.no-ip.info -port 1099 -local-host $LOCAL_HOST -agents VMAgent:dtp.jade.vm.VMAgent
